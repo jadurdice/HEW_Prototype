e70e1613479fab3dcc7cb8f7dde3f273a2075578
Liuei Feng <jadur.d@gmail.com>
2019-10-27 01:21:04 +0900

    水流完成
    20191027

---
+++ Assets/Scripts/Stream.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stream : MonoBehaviour
+{
+    public Transform flowPoint;
+    public float streamForce;
+
+    protected Vector3 streamDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        streamDirection = Vector3.Normalize(flowPoint.position - transform.position);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<Rigidbody>().AddForce(streamDirection * streamForce);
+        }
+
+    }
+
+
+}
