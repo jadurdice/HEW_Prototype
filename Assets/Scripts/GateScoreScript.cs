1a669cd6f456edd22bede67feb786d230b7da0aa
小野牧人 <nextatoz@gmail.com>
2019-10-26 17:16:28 +0900

    スコア処理

---
+++ Assets/Scripts/GateScoreScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScoreScript : MonoBehaviour
+{
+    private bool plus;
+
+
+
+    public GameObject Score_Controll;
+    ScoreControll ScoreScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        plus = false;
+
+        Score_Controll = GameObject.Find("Score_Controll");
+        ScoreScript = Score_Controll.GetComponent<ScoreControll>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.CompareTag("Player")&&!plus)
+        {
+           ScoreScript.Score += 200;
+
+            plus = true;
+        }
+        
+
+    }
+
+}
---
+++ Assets/Scripts/ScoreControll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreControll : MonoBehaviour
+{
+    public Text Score_Text; //テキストオブジェクトを取得
+   public int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score_Text.text = Score.ToString();
+    }
+}
